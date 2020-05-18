4edc0655b1cdb10791415279e503e356a9be3efa
Carl Cronsioe <carl.cronsioe@kambi.com>
2020-05-18 09:00:07 +0200

    lite snyggare

--- pages/games/[gameId].js
+++ pages/games/[gameId].js
@@ -275,7 +275,10 @@ export default function GameScreen() {
           {guesses
             .filter((guess) => guess.round != round)
             .map((guess) => (
-              <div className="flex justify-between mt-1" key={guess.round}>
+              <div
+                className="flex justify-between mt-1 px-10 "
+                key={guess.round}
+              >
                 <Guess
                   reactions={guess.reactions?.player1}
                   selectedPlayer="player1"
