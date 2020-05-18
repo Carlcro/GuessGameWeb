db9d76be0bddcbc2483f160211bd67d1a72c5fcf
Carl Cronsioe <carl.cronsioe@kambi.com>
2020-05-18 15:50:42 +0200

    PWA test

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
 /* next.config.js */
 const webpack = require("webpack");
 const withImages = require("next-images");
+const withPWA = require("next-pwa");
+
 require("dotenv").config();
-module.exports = withImages({
-  webpack: (config) => {
-    const env = Object.keys(process.env).reduce((acc, curr) => {
-      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
-      return acc;
-    }, {});
-    config.plugins.push(new webpack.DefinePlugin(env));
-    return config;
-  },
-});
+module.exports = withPWA(
+  withImages({
+    pwa: {
+      dest: "public",
+    },
+    webpack: (config) => {
+      const env = Object.keys(process.env).reduce((acc, curr) => {
+        acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+        return acc;
+      }, {});
+      config.plugins.push(new webpack.DefinePlugin(env));
+
+      return config;
+    },
+  })
+);
