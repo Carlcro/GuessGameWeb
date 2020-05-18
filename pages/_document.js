955b93c48de0c89b270a2ee711edcd62b03b5396
Carl Cronsioe <carl.cronsioe@kambi.com>
2020-05-18 17:12:38 +0200

    lite bättre pwa

--- pages/_document.js
+++ pages/_document.js
@@ -13,14 +13,14 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta name="application-name" content="PWA App" />
+          <meta name="application-name" content="Mind Merge" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
             name="apple-mobile-web-app-status-bar-style"
             content="default"
           />
-          <meta name="apple-mobile-web-app-title" content="PWA App" />
-          <meta name="description" content="Best PWA App in the world" />
+          <meta name="apple-mobile-web-app-title" content="Mind Merge" />
+          <meta name="description" content="Mind Merge Game" />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="theme-color" content="#000000" />
@@ -29,34 +29,9 @@ class MyDocument extends Document {
             content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
           />
 
-          <link rel="128x128" sizes="128x128" href="/128x128.png" />
           <link rel="icon" href="/favicon.ico" />
-
           <link rel="manifest" href="/manifest.json" />
-
           <link rel="shortcut icon" href="/favicon.ico" />
-
-          {/* <meta name="twitter:card" content="summary" />
-          <meta name="twitter:url" content="https://yourdomain.com" />
-          <meta name="twitter:title" content="PWA App" />
-          <meta
-            name="twitter:description"
-            content="Best PWA App in the world"
-          />
-          <meta
-            name="twitter:image"
-            content="https://yourdomain.com/static/icons/android-chrome-192x192.png"
-          />
-          <meta name="twitter:creator" content="@DavidWShadow" />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="PWA App" />
-          <meta property="og:description" content="Best PWA App in the world" />
-          <meta property="og:site_name" content="PWA App" />
-          <meta property="og:url" content="https://yourdomain.com" />
-          <meta
-            property="og:image"
-            content="https://yourdomain.com/static/icons/apple-touch-icon.png"
-          /> */}
         </Head>
         <body>
           <Main />
