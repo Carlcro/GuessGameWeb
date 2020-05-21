8b4236b682c31903908b505b15cc4594cbc54649
Carl Cronsioe <carl.cronsioe@kambi.com>
2020-05-21 13:13:40 +0200

    landing page layout

--- pages/index.js
+++ pages/index.js
@@ -53,67 +53,64 @@ export default function IndexPage() {
         <title>Mind Merge - Merge your brains!</title>
       </Head>
       <section>
-        <div id="container" className="h-full px-8">
-          <div className="flex justify-center content-center  items-center">
-            <div className="w-full lg:w-4/5 flex flex-col sm:flex-row">
-              <div>
-                <h1 className="text-headline text-5xl">
-                  Welcome to Mind Merge!
-                </h1>
-                <h2 className="text-paragraph text-3xl md:w-3/5">
-                  The game where you work together to merge your brains 🤯. Join
-                  up with a friend, work your way to reach the same word and win
-                  🚀!
-                </h2>
-              </div>
-              <div className="flex flex-col h-full mt-5 bg-paragraph rounded p-3">
-                <div className="flex flex-col">
-                  <form className="flex flex-col text-stroke">
-                    <label htmlFor="email">Email</label>
-                    <input
-                      id="email"
-                      className="p-2 rounded"
-                      type="email"
-                      onChange={({ target }) => setEmail(target.value)}
-                    ></input>
-                    <label className="mt-2" htmlFor="name">
-                      First Name
-                    </label>
-                    <input
-                      id="name"
-                      className="p-2 rounded"
-                      type="text"
-                      onChange={({ target }) => setName(target.value)}
-                    ></input>
-                    <label className="mt-2" htmlFor="password">
-                      Password
-                    </label>
-                    <input
-                      id="password"
-                      className="p-2 rounded"
-                      type="password"
-                      onChange={({ target }) => setPassword(target.value)}
-                    ></input>
-                    <label className="mt-2" htmlFor="repeatPassword">
-                      Repeat Password
-                    </label>
-                    <input
-                      id="repeatPassword"
-                      className="p-2 rounded"
-                      type="password"
-                      onChange={({ target }) => setRePassword(target.value)}
-                    ></input>
-                    <button
-                      id="submit"
-                      className="bg-button text-buttonText p-3 mt-3 rounded"
-                      type="submit"
-                      onClick={handleSignUp}
-                    >
-                      Sign Up
-                    </button>
-                  </form>
-                  <span className="text-stroke mt-4">{error}</span>
-                </div>
+        <div id="container" className="h-full px-10 lg:px-20">
+          <div className="w-full flex justify-between flex-col sm:flex-row">
+            <div className="w-full sm:w-3/5">
+              <h1 className="text-headline text-5xl">Welcome to Mind Merge!</h1>
+              <h2 className="text-paragraph text-3xl">
+                The game where you work together to merge your brains 🤯. Join
+                up with a friend, work your way to reach the same word and win
+                🚀!
+              </h2>
+            </div>
+            <div className="flex flex-1 flex-col h-full mt-5 bg-paragraph rounded p-3 max-w-xs">
+              <div className="flex flex-col">
+                <h2 className="text-2xl text-stroke">Sign Up</h2>
+                <form className="flex flex-col text-stroke">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    id="email"
+                    className="p-2 rounded"
+                    type="email"
+                    onChange={({ target }) => setEmail(target.value)}
+                  ></input>
+                  <label className="mt-2" htmlFor="name">
+                    First Name
+                  </label>
+                  <input
+                    id="name"
+                    className="p-2 rounded"
+                    type="text"
+                    onChange={({ target }) => setName(target.value)}
+                  ></input>
+                  <label className="mt-2" htmlFor="password">
+                    Password
+                  </label>
+                  <input
+                    id="password"
+                    className="p-2 rounded"
+                    type="password"
+                    onChange={({ target }) => setPassword(target.value)}
+                  ></input>
+                  <label className="mt-2" htmlFor="repeatPassword">
+                    Repeat Password
+                  </label>
+                  <input
+                    id="repeatPassword"
+                    className="p-2 rounded"
+                    type="password"
+                    onChange={({ target }) => setRePassword(target.value)}
+                  ></input>
+                  <button
+                    id="submit"
+                    className="bg-button text-buttonText p-3 mt-3 rounded"
+                    type="submit"
+                    onClick={handleSignUp}
+                  >
+                    Sign Up
+                  </button>
+                </form>
+                <span className="text-stroke mt-4">{error}</span>
               </div>
             </div>
           </div>
