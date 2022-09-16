2492a5dd99dd767659095ffc675b0f27986fd7e9
Carl Cronsioe <carlcro@kth.se>
2022-09-16 22:59:17 +0200

    Bättre

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -19,13 +19,14 @@ firebase.initializeApp(firebaseConfig);
 // Retrieve firebase messaging
 if (typeof window !== "undefined") {
   const messaging = firebase.messaging();
+  messaging.onBackgroundMessage(function (payload) {
+    console.log("Received background message ", payload);
+
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+    };
+  });
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 }
-
-messaging.onBackgroundMessage(function (payload) {
-  console.log("Received background message ", payload);
-
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-  };
-});
